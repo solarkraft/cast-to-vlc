41682afb3a7952a72b164f06459cb86832cff6bf
Paul <paul@hendrix.io>
2019-03-11 20:07:15 +0100

    Move to asynchronous architecture

--- vlcmanager.py
+++ vlcmanager.py
@@ -1,29 +1,68 @@
 #Interfaces with VLC through the LUA CL
 #Removes last playlist item when detected that VLC can't play it (otherwise causes endless errors)
 
-import subprocess
+import asyncio
+import websockets
 import time
 
-#command = ["./slowprogram.sh"]
-#command = ["./echo.sh"]
-command = ["script", "-c cvlc --control cli"]
+process = "vlc process one day"
 
-with subprocess.Popen(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, bufsize=1, shell=False) as p:
-    def send(command):
-        p.stdin.write(bytes(command, 'utf-8') + b'\n')
-        p.stdin.flush()
+async def read_stream(stream, cb):  
+    while True:
+        line = await stream.readline()
+        if line:
+            cb(line)
+        else:
+            break  
 
-    for line in iter(p.stdout.readline, b''):
-        print(str(line, 'utf-8').strip('\n'))
-        #time.sleep(0.1)
+async def monitor_vlc():
+    command = ["script", "-c cvlc --control cli"]
 
-        if line.__contains__(b"Command Line Interface initialized"):
-            #time.sleep(1)
-            send("add abcd")
+    process = await asyncio.create_subprocess_exec(*command,
+            stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE, stdin=asyncio.subprocess.PIPE)
 
-        if line.__contains__(b"main input error"):
-            send("stop")
+    def send_command(command):
+        print("Sending command: "+ command)
+        process.stdin.write(bytes(command, 'utf-8') + b'\n')
+
+    def handle_output(output_raw):
+        output = str(output_raw, "utf-8").strip('\n')
+
+        print(output)
+
+        #auto-stop
+        if output.__contains__("main input error"):
+            send_command("stop")
+
+        #test auto-stop
+        if output.__contains__("initialized"):
+            time.sleep(2)
+            send_command("add a")
 
 
-        
-p.wait()
+    await asyncio.wait([
+        #read_stream(process.stdout, lambda x: print("STDOUT: %s" % x)),
+        read_stream(process.stdout, handle_output),
+        read_stream(process.stderr, lambda x: print("STDERR: %s" % x))
+    ])
+
+    return await process.wait()
+
+
+async def other():
+    while True:
+        print("henlo")
+        await asyncio.sleep(10)
+
+if __name__ == '__main__':
+
+    #command = ["./slowprogram.sh"]
+    #command = ["./echo.sh"]
+
+    loop = asyncio.get_event_loop()
+
+    loop.create_task(monitor_vlc())
+    loop.create_task(other())
+
+    loop.run_forever()
+    loop.close()
